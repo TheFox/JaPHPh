549d89b88541f1466c1e993e71c4ea5779d08eae
TheFox <github@fox21.at>
2012-01-28 18:36:27 +0100

    Initial commit.

---
+++ japhph.php
@@ -0,0 +1,25 @@
+<?php
+
+// Created @ 28.01.2012 by TheFox@fox21.at
+
+/*
+thefox:> php --version
+PHP 5.3.6 with Suhosin-Patch (cli) (built: Sep  8 2011 19:34:00) 
+Copyright (c) 1997-2011 The PHP Group
+Zend Engine v2.3.0, Copyright (c) 1998-2011 Zend Technologies
+*/
+
+class p{
+	
+	public function __call($c, $a){
+		print ''.($this->x .= $c)."\n";
+    }
+    
+}
+
+$p = new p();
+
+$p->r($p->e($p->k($p->c($p->a($p->h($p->PHP($p->r($p->e($p->h($p->t($p->o($p->n($p->A($p->t($p->s($p->u($p->J())))))))))))))))));
+
+
+?>
